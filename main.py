765bdc2bbe8c0d5efba8623d6e7300076fca8592
chichichen10 <37727069+chichichen10@users.noreply.github.com>
2021-11-06 19:54:41 +0000

    bugs fixed

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 import os
 import asyncio
 
-
 client = discord.Client()
 
 verbose = True
@@ -14,25 +13,43 @@ verbose = True
 async def on_ready():
     print('目前登入身份：', client.user)
 
+
 @client.event
 async def on_voice_state_update(member, before, after):
     user = member
+    if (user == client.user):
+        return
     voice_channel = None
-    if(user.voice):
+    if (user.voice):
         voice_channel = user.voice.channel
     guild = user.guild
     for vc in guild.voice_channels:
-            if vc.name == '紅隊' or vc.name == '藍隊':
-                if(not vc.members):
-                    await vc.delete()
+        if vc.name == '紅隊' or vc.name == '藍隊' or vc.name == '廁所':
+            if (not vc.members):
+                await vc.delete()
 
-    
     if (voice_channel != None and before.channel != after.channel):
+        player_ids = [x.id for x in voice_channel.members]
+        if (user.id == 429657581313720321):
+            try:
+                vc = await voice_channel.connect()
+                await asyncio.sleep(0.2)
+                audio_source = discord.FFmpegPCMAudio('recording.webm')
+                vc.play(audio_source, after=None)
+                vc.source.volume = 10.0
+                await asyncio.sleep(2)
+                await vc.disconnect()
+            except:
+                print('erroR')
+            return
+        elif(429657581313720321 in player_ids):
+            return
         try:
             vc = await voice_channel.connect()
             await asyncio.sleep(0.2)
-            audio_source = discord.FFmpegPCMAudio('voice_'+str(random.choice(range(1,17)))+'.mp3')
-            vc.play(audio_source,after=None)
+            audio_source = discord.FFmpegPCMAudio(
+                'voice_' + str(random.choice(range(1, 17))) + '.mp3')
+            vc.play(audio_source, after=None)
             vc.source.volume = 10.0
             await asyncio.sleep(2)
             await vc.disconnect()
@@ -55,21 +72,21 @@ async def on_message(message):
         else:
             await message.channel.send('到底想怎樣')
         return
-    
+
     if message.content == '!阿致回來':
         if not verbose:
             await message.channel.send('想不到吧')
             verbose = True
             user = message.author
             voice_channel = None
-            if(user.voice):
+            if (user.voice):
                 voice_channel = user.voice.channel
             if (voice_channel != None):
                 try:
                     vc = await voice_channel.connect()
                     await asyncio.sleep(0.2)
                     audio_source = discord.FFmpegPCMAudio('voice_15.mp3')
-                    vc.play(audio_source,after=None)
+                    vc.play(audio_source, after=None)
                     vc.source.volume = 10.0
                     await asyncio.sleep(2)
                     await vc.disconnect()
@@ -92,11 +109,15 @@ async def on_message(message):
     # if 'chi' in message.content.lower() and verbose:
     #     await message.channel.send('閉嘴啦魂爆')
     #     return
-    
+
     if message.content.startswith('!把') and message.content.endswith('關廁所'):
-        for member in mentions:
-            await member.move_to(client.get_channel(876115972325507072))
-            await message.channel.send('<@'+str(member.id)+'> 下去')
+        guild = message.author.guild
+        channel = await guild.create_voice_channel('廁所')
+        for player in mentions:
+            if player.voice:
+                await player.move_to(channel)
+                await message.channel.send('<@' + str(player.id) + '> 下去')
+
         return
 
     if message.content == '!阿致講話':
@@ -109,8 +130,9 @@ async def on_message(message):
             try:
                 vc = await voice_channel.connect()
                 await asyncio.sleep(0.5)
-                audio_source = discord.FFmpegPCMAudio('voice_'+str(random.choice(range(1,17)))+'.mp3')
-                vc.play(audio_source,after=None)
+                audio_source = discord.FFmpegPCMAudio(
+                    'voice_' + str(random.choice(range(1, 17))) + '.mp3')
+                vc.play(audio_source, after=None)
                 vc.source.volume = 10.0
                 # while not player.is_done():
                 #     await asyncio.sleep(1)
@@ -120,7 +142,7 @@ async def on_message(message):
             except:
                 print('erroR')
         return
-    
+
     if message.content == '!垃圾遊戲':
         user = message.author
         voice_channel = user.voice.channel
@@ -129,7 +151,7 @@ async def on_message(message):
                 vc = await voice_channel.connect()
                 await asyncio.sleep(0.5)
                 audio_source = discord.FFmpegPCMAudio('voice_2.mp3')
-                vc.play(audio_source,after=None)
+                vc.play(audio_source, after=None)
                 vc.source.volume = 10.0
                 # while not player.is_done():
                 #     await asyncio.sleep(1)
@@ -139,29 +161,35 @@ async def on_message(message):
             except:
                 print('erroR')
         return
-      
+
     if '!阿致分隊' in message.content:
         if message.content == '!阿致分隊 --n':
-            l = [1,2,3,4,5,6,7,8,9,10]
+            l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
             random.shuffle(l)
             blue = []
             red = []
             for i in range(10):
-                if(i<5):
+                if (i < 5):
                     blue.append(l[i])
                 else:
                     red.append(l[i])
-            await message.channel.send('藍方: '+' ,'.join(str(x) for x in sorted(blue)))
-            await message.channel.send('紅方: '+' ,'.join(str(x) for x in sorted(red)))
-        elif len(mentions)==10:
+            await message.channel.send('藍方: ' +
+                                       ' ,'.join(str(x) for x in sorted(blue)))
+            await message.channel.send('紅方: ' +
+                                       ' ,'.join(str(x) for x in sorted(red)))
+        elif len(mentions) == 10:
             guild = message.author.guild
             random.shuffle(mentions)
-            await message.channel.send('藍方: '+' ,'.join('<@'+str(x.id)+'> ' for x in mentions[:5]))
-            await message.channel.send('紅方: '+' ,'.join('<@'+str(x.id)+'> ' for x in mentions[5:]))
+            await message.channel.send('藍方: ' +
+                                       ' ,'.join('<@' + str(x.id) + '> '
+                                                 for x in mentions[:5]))
+            await message.channel.send('紅方: ' +
+                                       ' ,'.join('<@' + str(x.id) + '> '
+                                                 for x in mentions[5:]))
             new_msg = await message.channel.send('倒數5秒')
             for i in range(5):
                 await asyncio.sleep(1)
-                await new_msg.edit(content='倒數'+str(4-i)+'秒')
+                await new_msg.edit(content='倒數' + str(4 - i) + '秒')
             await asyncio.sleep(1)
             await new_msg.edit(content='下去')
             blue_channel = await guild.create_voice_channel('藍隊')
@@ -172,7 +200,7 @@ async def on_message(message):
             for player in mentions[5:]:
                 if player.voice:
                     await player.move_to(red_channel)
-        
+
         else:
             user = message.author
             if not user.voice:
@@ -187,14 +215,18 @@ async def on_message(message):
                     if m in players:
                         players.remove(m)
             print(len(players))
-            if len(players)==10:
+            if len(players) == 10:
                 random.shuffle(players)
-                await message.channel.send('藍方: '+' ,'.join('<@'+str(x.id)+'> ' for x in players[:5]))
-                await message.channel.send('紅方: '+' ,'.join('<@'+str(x.id)+'> ' for x in players[5:]))
+                await message.channel.send('藍方: ' +
+                                           ' ,'.join('<@' + str(x.id) + '> '
+                                                     for x in players[:5]))
+                await message.channel.send('紅方: ' +
+                                           ' ,'.join('<@' + str(x.id) + '> '
+                                                     for x in players[5:]))
                 new_msg = await message.channel.send('倒數5秒')
                 for i in range(5):
                     await asyncio.sleep(1)
-                    await new_msg.edit(content='倒數'+str(4-i)+'秒')
+                    await new_msg.edit(content='倒數' + str(4 - i) + '秒')
                 await asyncio.sleep(1)
                 await new_msg.edit(content='下去')
                 blue_channel = await guild.create_voice_channel('藍隊')
@@ -205,18 +237,19 @@ async def on_message(message):
                 for player in players[5:]:
                     if player.voice:
                         await player.move_to(red_channel)
-            elif len(players)<10:
+            elif len(players) < 10:
                 await message.channel.send('人不夠 北七')
-            elif len(players)>10:
+            elif len(players) > 10:
                 await message.channel.send('人山人海 北七')
         return
 
     if message.content == '!阿致挖礦':
-        await message.channel.send('邁向財富自由\nhttps://max.maicoin.com/signup?r=1448ee5b')
+        await message.channel.send(
+            '邁向財富自由\nhttps://max.maicoin.com/signup?r=1448ee5b')
         return
 
     if message.content == '!阿致能幹嘛':
-        help_msg='''
+        help_msg = '''
 
         !阿致嘴閉閉: 讓阿致少講點話
 !阿致回來: 本來的阿致
@@ -241,13 +274,11 @@ https://github.com/chichichen10/Magic_Shell_dcbot
         '''
         await message.channel.send(help_msg)
         return
-        
 
-
-    a_msg = ['哭啊', '不是誒老哥', '誒你剛有看到嗎 我剛很強吧', '外掛啦外掛', '這對面很有水準誒','我要吐了']
+    a_msg = ['哭啊', '不是誒老哥', '誒你剛有看到嗎 我剛很強吧', '外掛啦外掛', '這對面很有水準誒', '我要吐了']
 
     if random.choice([1, 2, 3]) == 1 and verbose:
         await message.channel.send(random.choice(a_msg))
 
-client.run(os.environ['BOT_TOKEN'])
 
+client.run(os.environ['BOT_TOKEN'])
