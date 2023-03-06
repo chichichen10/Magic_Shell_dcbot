41d589cf276bff172c11a827db94bccd8a6d88f6
chichichen10 <>
2023-03-06 04:26:54 +0000

    remove log folder

--- logger.py
+++ logger.py
@@ -1,16 +1,25 @@
 import logging
 import datetime
 
+
 class logger:
     tz = datetime.timezone(datetime.timedelta(hours=+8))
     FORMAT = '%(asctime)s %(levelname)s: %(message)s'
-    logging.basicConfig(level=logging.INFO, filename='log/'+str(datetime.datetime.now(tz).date())+'.log', filemode='a', format=FORMAT, datefmt='%m/%d/%Y %I:%M:%S +0800')
+    logging.basicConfig(level=logging.INFO,
+                        filename='log/' +
+                        str(datetime.datetime.now(tz).date()) + '.log',
+                        filemode='a',
+                        format=FORMAT,
+                        datefmt='%m/%d/%Y %I:%M:%S')
+
     def info(msg):
         logging.info(msg)
         print('[INFO] ' + msg)
+
     def warn(msg):
         logging.warning(msg)
-        print('[WARNING] '+msg)
+        print('[WARNING] ' + msg)
+
     def err(msg):
         logging.error(msg)
-        print('[ERROR]'+msg)
+        print('[ERROR]' + msg)
