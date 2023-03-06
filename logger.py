1ae167332f9b762ffd64efcd5de070b1940a88ce
chichichen10 <>
2023-03-06 09:48:55 +0000

    restructure log file

--- logger.py
+++ logger.py
@@ -1,16 +1,23 @@
 import logging
 import datetime
+from pytz import timezone
 
 
 class logger:
     tz = datetime.timezone(datetime.timedelta(hours=+8))
+
+    def timetz(*args):
+        return datetime.datetime.now(timezone('Asia/Taipei')).timetuple()
+
+    # tz = timezone('Asia/Shanghai')
+    logging.Formatter.converter = timetz
     FORMAT = '%(asctime)s %(levelname)s: %(message)s'
     logging.basicConfig(level=logging.INFO,
-                        filename='log/' +
-                        str(datetime.datetime.now(tz).date()) + '.log',
+                        filename='log/' + str(datetime.datetime.now(tz).year) +
+                        '/' + str(datetime.datetime.now(tz).month).zfill(2) +
+                        '/' + str(datetime.datetime.now(tz).date()) + '.log',
                         filemode='a',
-                        format=FORMAT,
-                        datefmt='%m/%d/%Y %I:%M:%S')
+                        format=FORMAT)
 
     def info(msg):
         logging.info(msg)
--- message_handler.py
+++ message_handler.py
@@ -5,27 +5,35 @@ from asgiref.sync import sync_to_async
 import datetime
 from logger import logger as log
 
+
 async def messageHandler(message: discord.Message, bot_user: discord.Member):
     msg = message.content
-    if(len(msg)>100):
+    if (len(msg) > 100):
         await message.channel.send('太長了，請節約資源')
         return
     openai.api_key = os.environ['OPEN_AI_API_KEY']
-    pre_messages = [{"role": "assistant" if m.author == bot_user else "user", "content": m.content if len(m.content)<=100 else ""} async for m in message.channel.history(limit=15,after=message.created_at - datetime.timedelta(minutes=5),oldest_first=False)]
+    pre_messages = [{
+        "role": "assistant" if m.author == bot_user else "user",
+        "content": m.content if len(m.content) <= 100 else ""
+    } async for m in message.channel.history(limit=15,
+                                             after=message.created_at -
+                                             datetime.timedelta(minutes=5),
+                                             oldest_first=False)]
     pre_messages.reverse()
     print(pre_messages)
-    system_message = [{"role": "system", "content": "以下對話都用繁體中文，長度不要超過50個字，你是阿致，現在在玉山銀行當ARM，脾氣暴躁，口氣不佳，現在在一個dicord群駔，有人發送訊息，你要回應他。在回應中可以加入最多1個以下的口頭禪：「哭啊」、「我要吐了」、「不是誒老哥」、「有一說一」、「傑哥你說對不對」，一次回答中不要加入超過一個口頭禪，回答中可以加入一些髒話，直接講出你要講的話，不要任何冒號或引號。"}]
-    res = await sync_to_async(openai.ChatCompletion.create)(
-        model="gpt-3.5-turbo",
-        messages= system_message + pre_messages,
-        temperature = 1.1,
-        max_tokens= 300,
-        presence_penalty= 1,
-        frequency_penalty= 1
-       
-    )
-    answer = res.choices[0]['message']['content']  
+    system_message = [{
+        "role":
+        "system",
+        "content":
+        "以下對話都用繁體中文，長度不要超過50個字，你是阿致，現在在玉山銀行當ARM，脾氣暴躁，口氣不佳，現在在一個dicord群駔，有人發送訊息，你要回應他。在回應中可以加入最多1個以下的口頭禪：「哭啊」、「我要吐了」、「不是誒老哥」、「有一說一」、「傑哥你說對不對」，一次回答中不要加入超過一個口頭禪，回答中可以加入一些髒話，直接講出你要講的話，不要任何冒號或引號。"
+    }]
+    res = await sync_to_async(openai.ChatCompletion.create
+                              )(model="gpt-3.5-turbo",
+                                messages=system_message + pre_messages,
+                                temperature=1.1,
+                                max_tokens=300,
+                                presence_penalty=1,
+                                frequency_penalty=1)
+    answer = res.choices[0]['message']['content']
     await message.channel.send(answer)
     log.info("Tokens used: " + str(res.usage.total_tokens))
-    
-
